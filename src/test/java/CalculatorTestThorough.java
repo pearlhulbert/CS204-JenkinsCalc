8b81dcd0b8ea16343ee0c18384bffd8cc2353688
clamguts <pearl.k.hulbert@gmail.com>
2023-03-06 11:19:31 -0700

    new comment

--- src/test/java/CalculatorTestThorough.java
+++ src/test/java/CalculatorTestThorough.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.*;
 
 public class CalculatorTestThorough {
 
+    // new comment
     @Test
     public void addTest(){
         Calculator myCalculator = new Calculator();
